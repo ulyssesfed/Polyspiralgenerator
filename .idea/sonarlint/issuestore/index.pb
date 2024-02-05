433c0aca963f3d8adab4ad24f302a53fadf5a7e3
ulyfl <ulyfleen@gmail.com>
2024-02-05 18:36:25 +0000

    database removed

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 S
 #.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+7
+main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
+Å
+Q.idea/sonarlint/securityhotspotstore/f/1/f1bdda93d9a278e358509d498e17d97764c1fb29,b\5\b58a0677868503201074e965423d875803239017
--- main.py
+++ main.py
@@ -1,43 +1,9 @@
 import colorsys
 import turtle
 
-import pyodbc
 from turtle import *
 from random import randint
 import time
-
-
-def get_configs():
-    # Connect to the Access database
-    conn = pyodbc.connect(
-        r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\uly\PycharmProjects\turtle\patterns.accdb")
-    cursor = conn.cursor()
-
-    # Define the table name
-    table_name = "patterns"
-
-    # Retrieve the configs from the database
-    configs = []
-    for row in cursor.execute(f"SELECT * FROM {table_name}"):
-        config = {
-            "angle": row[0],
-            "pensize": row[1],
-            "colour1": row[2],
-            "colour2": row[3],
-            "colour3": row[4],
-            "colour12": row[6],
-            "colour22": row[7],
-            "colour32": row[8],
-            "rand": row[5],
-            "descriptions": row[9]
-        }
-        configs.append(config)
-
-    # Close the connection
-    cursor.close()
-    conn.close()
-
-    return configs
 
 
 def turtlefunc():
@@ -47,7 +13,7 @@ def turtlefunc():
     t.goto(0, 0)
     t.pendown()
     colormode(255)
-    duration = 10
+    duration = randint(20, 95)
     angle = randint(48, 360)
     t.pensize(randint(2, 4))
     colour1 = randint(0, 255)
@@ -56,33 +22,8 @@ def turtlefunc():
     colour12 = randint(0, 255)
     colour22 = randint(0, 255)
     colour32 = randint(0, 255)
-    rand = 1300
     pencolor(0, 0, 0)
     j = 0
-
-    configs = get_configs()
-
-    if configs:
-        print("Existing configs:")
-        for i, config in enumerate(configs):
-            print(f"{i + 1}. {config}")
-        choice = input("Enter the number of the config to load, or press enter to create a new one: ")
-        if choice:
-            choice = int(choice) - 1
-            if 0 <= choice < len(configs):
-                config = configs[choice]
-                angle = config["angle"]
-                t.pensize(config["pensize"])
-                colour1 = config["colour1"]
-                colour2 = config["colour2"]
-                colour3 = config["colour3"]
-                colour12 = config["colour12"]
-                colour22 = config["colour22"]
-                colour32 = config["colour32"]
-                rand = config["rand"]
-                duration = int(input("how long do you want it to go"))
-    else:
-        print("No existing configs.")
 
     start_time = time.time()
 
@@ -92,6 +33,22 @@ def turtlefunc():
     end_color = (colour12, colour22, colour32)
 
     rainbowMode = input("Do you want to use rainbow mode? (y/n) ")
+
+    CustomValue = input("Do you want to use custom values? (y/n) ")
+
+    if CustomValue.lower() == "y":
+
+        duration = int(input("Enter the duration for the turtle (in seconds): "))
+        angle = int(input("Enter the angle for the turtle (0-360): "))
+        t.pensize(int(input("Enter the pen size for the turtle (1-10): ")))
+        colour1 = int(input("Enter the value for the first colour (0-255): "))
+        colour2 = int(input("Enter the value for the second colour (0-255): "))
+        colour3 = int(input("Enter the value for the third colour (0-255): "))
+        colour12 = int(input("Enter the value for the first colour (0-255): "))
+        colour22 = int(input("Enter the value for the second colour (0-255): "))
+        colour32 = int(input("Enter the value for the third colour (0-255): "))
+        start_color = (colour1, colour2, colour3)
+        end_color = (colour12, colour22, colour32)
 
     if rainbowMode.lower() == "n":
 
@@ -120,6 +77,7 @@ def turtlefunc():
             # Update the start time for the next cycle
             start_time += cycle_duration
         t.hideturtle()
+        turtle.exitonclick()
 
 
     else:
@@ -137,49 +95,11 @@ def turtlefunc():
             t.left(angle)
             t.pencolor(r, g, b)
         t.hideturtle()
-
-
-    record_values = input(
-        "Do you want to record the values of angle, pensize, colour1, colour2, and colour3 in a database? (y/n) ")
-
-    if record_values.lower() == "y":
-
-        descriptions = input("Enter a description for the config: ")
-        # Connect to the Access database
-        conn = pyodbc.connect(
-            r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\uly\PycharmProjects\turtle\patterns.accdb")
-        cursor = conn.cursor()
-
-        # Define the table and field names
-        table_name = "patterns"
-        field_names = ["angle", "pensize", "colour1", "colour2", "colour3", "colour12", "colour22", "colour32", "rand", "descriptions"]
-
-        # Create the table if it doesn't exist
-        try:
-            cursor.execute(f"CREATE TABLE {table_name} ({', '.join([f'{name} INTEGER' for name in field_names])})")
-        except pyodbc.ProgrammingError:
-            pass
-
-        # Insert the values into the table
-        values = [angle, t.pensize(), colour1, colour2, colour3, colour12, colour22, colour32, rand, descriptions]
-        cursor.execute(
-            f"INSERT INTO {table_name} ({', '.join(field_names)}) VALUES ({', '.join(['?' for i in range(len(values))])})",
-            values)
-        conn.commit()
-
-        # Close the connection
-        cursor.close()
-        conn.close()
-
-        print("Values recorded in database.")
-
-
-    else:
-        print("Values not recorded.")
-
+        turtle.exitonclick()
 
     screen = Screen()
     screen.bye()
+
 
 if __name__ == '__main__':
     turtlefunc()
